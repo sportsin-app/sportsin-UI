f76a9fac89571fde0eeed88017dc5cb447dcd61e
manish <manishmishra499@gmail.com>
2020-07-14 23:33:31 +0530

    paytm gateway checksum added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ServiceProviderFormComponent } from './booking-form/service-provider-form/service-provider-form.component';
 import { InvitationComponent } from './invitation/invitation.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminUserFormComponent } from './admin-user-form/admin-user-form.component';
+import { PaymentGatewayComponent } from './payment-gateway/payment-gateway.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full'},
+  { path: 'registerUser', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent},
   { path: 'app-booking-form', component: BookingFormComponent },
   { path: 'app-service-provider-form', component: ServiceProviderFormComponent},
-  { path: 'app-invitation', component: InvitationComponent}
+  { path: 'app-invitation', component: InvitationComponent},
+  { path: 'app-adminUser', component: AdminUserFormComponent },
+  { path: 'app-payment-gateway', component: PaymentGatewayComponent}
   // { path: '**', component: P }
 ];
 
