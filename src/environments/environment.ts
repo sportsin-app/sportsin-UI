294cb4bc639733fa0c6b69597459182fa0d35379
sportsin-app <61380311+sportsin-app@users.noreply.github.com>
2020-10-28 09:09:54 +0530

    Update environment.ts
    removed unwanted changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,17 @@
 
 export const environment = {
   production: false,
-  payment_key:'rzp_test_UbEXmswyXSTxGB',
-  payment_secret: 'ReDU41JbmJG2bsh4sDZ7zyln',
+  payment_key:'',
+  payment_secret: '',
   firebaseConfig: {
-    apiKey: "AIzaSyCWEIs1CU7P7F1JYWxZGX56P1uzKAUlu9Y",
-    authDomain: "sportsin-test-a.firebaseapp.com",
-    databaseURL: "https://sportsin-test-a.firebaseio.com",
-    projectId: "sportsin-test-a",
-    storageBucket: "sportsin-test-a.appspot.com",
-    messagingSenderId: "797690304874",
-    appId: "1:797690304874:web:18b531ae1c910e67962d13",
-    measurementId: "G-163LJDH886"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
